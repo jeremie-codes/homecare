3c2594c8767ad74af71a8fb58cb1fc798c9a64c6
jeremie-codes <jrmmianda@gmail.com>
2025-10-24 04:19:55 +0100

    config filsystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => public_path('storage'),
             'serve' => true,
             'throw' => false,
             'report' => false,
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -74,7 +74,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage''storage') => storage_path('app/public'),
     ],
 
 ];
