014256c881fd7a9c6847c8e62f85ea8bb85295a0
jeremie-codes <jrmmianda@gmail.com>
2025-10-27 16:48:56 +0100

    config filsystem

--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -18,7 +18,7 @@ class UsersTable
             ->columns([
                 ImageColumn::make('avatar')->label('Image')->circular()
                 ->state(function ($record) {
-                    return $record->image ? asset($record->image) : null;
+                    return $record->avatar ? asset($record->avatar) : null;
                 }),
                 TextColumn::make('name')
                     ->searchable(),
