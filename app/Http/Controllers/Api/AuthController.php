80c9f5c74c13178035fd34c997f8504c83cac401
jeremie-codes <jrmmianda@gmail.com>
2025-11-03 17:39:13 +0100

    ajout de createOptionUsing

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -44,7 +44,7 @@ class AuthController extends Controller
         ]);
     }
 
-    public function signin(Request $request)
+    public function register(Request $request)
     {
         $request->validate([
             'name' => 'required|string',
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\Api\TaskController;
 // ---------------- AUTH ----------------
 Route::prefix('auth')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
 
 
