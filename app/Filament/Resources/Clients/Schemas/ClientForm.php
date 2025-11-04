2dd8597698ff895484feec728b911c8494a02b42
jeremie-codes <jrmmianda@gmail.com>
2025-11-04 11:27:08 +0100

    ajout de createOptionUsing

--- app/Filament/Resources/Clients/Schemas/ClientForm.php
+++ app/Filament/Resources/Clients/Schemas/ClientForm.php
@@ -68,7 +68,7 @@ class ClientForm
 
                             Select::make('type')
                                 ->label('Type de client')
-                                ->options(['personnel' => 'Personnel', 'entreprise' => 'Entreprise'])
+                                ->options(['particulier' => 'Particulier', 'entreprise' => 'Entreprise'])
                                 ->reactive()
                                 ->required(),
 
@@ -76,7 +76,7 @@ class ClientForm
                                 ->label('Nom de l\'entreprise')
                                 ->required()
                                 //->dehydrated(fn)
-                                ->hidden(fn ($record) => !empty($record["type"]) && $record["type"] == 'personnel')
+                                ->hidden(fn ($record) => !empty($record["type"]) && $record["type"] == 'particulier')
                                 ->default(null),
 
                     ])
