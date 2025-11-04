3da6de67b7150a45c600eef440b6f3a8fdda3f53
jeremie-codes <jrmmianda@gmail.com>
2025-11-04 18:22:17 +0100

    rapports

--- app/Models/Rapport.php
+++ app/Models/Rapport.php
@@ -17,6 +17,6 @@ class Rapport extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'auteur_id');
+        return $this->belongsTo(User::class);
     }
 }
